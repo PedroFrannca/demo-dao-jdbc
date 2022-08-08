1c1c60e6d0458a44ad09bd7a4f3cb553eb91a1b1
Pedro França <pedrofrannca@gmail.com>
2022-08-08 01:12:32 -0300

    bug fix and findById implemention in sellerDaoJDBC

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,11 +1,12 @@
 package model.dao;
 
+import db.DB;
 import model.dao.impl.SellerDaoJDBC;
 
 public class DaoFactory {
 
 	public static SellerDao createSellerDao() {
-		return new SellerDaoJDBC();
+		return new SellerDaoJDBC(DB.getConnection());
 	}
 	
 	/* o metodo tem o tipo de retorno a interface(SellerDao), 
